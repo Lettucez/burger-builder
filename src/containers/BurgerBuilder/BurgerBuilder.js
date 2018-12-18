9efff2ebbd224ab3c0f0315ff117f7d5276f029a
Lettucez <chrisl7186@gmail.com>
2018-12-18 15:48:29 -0500

    add burger ingredient css

---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,22 @@
+import React, {Component} from 'react';
+
+
+class BurgerBuilder extends Component {
+
+
+    render() {
+        return (
+            <>
+                <div>
+                    Burger
+                </div>
+                <div>
+                    Controls
+                </div>
+            </>
+        );
+    }
+
+}
+
+export default BurgerBuilder;
