f632297b76447355faf3c5e0e8bf2976a3c403ad
Lettucez <chrisl7186@gmail.com>
2018-12-20 15:31:07 -0500

    add addition build controls

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -1,10 +1,22 @@
 import React from 'react';
 
+import BuildControl from './BuildControl/BuildControl';
+import styles from './BuildControls.module.css';
+
+const controls = [
+    { label: 'Salad', type: 'salad' },
+    { label: 'Bacon', type: 'bacon' },
+    { label: 'Cheese', type: 'cheese' },
+    { label: 'Meat', type: 'meat' },
+];
+
 const buildControls = (props) => {
 
     return (
-        <div>
-
+        <div className={styles.BuildControls}>
+            { controls.map((control) => {
+                return <BuildControl key={control.label} label={control.label} added={() => props.ingredientAdded(control.type)} />;
+            }) }
         </div>
     );
 };
