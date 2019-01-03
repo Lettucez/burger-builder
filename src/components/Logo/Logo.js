fbf8a46f95ad65183a250c49245259e88e2ab79f
Christopher Leo <cleo@wvuf.org>
2019-01-03 17:34:48 -0500

    add navigation components

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import styles from './Logo.module.css';
 
 import burgerLogo from '../../assets/images/burger-logo.png'
 
 const logo = (props) => {
 
     return (
-        <div>
+        <div className={styles.Logo}>
             <img src={burgerLogo} alt="Burger Logo" />
         </div>
     );
